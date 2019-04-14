f6e40e5a48aae22ec86e89d84ccad83522d3e713
lgh7418 <lgh7418@gmial.com>
2019-04-14 20:33:48 +0900

    19. 홈페이지 구현

--- main.js
+++ main.js
@@ -5,37 +5,65 @@ var url = require("url"); // 모듈 url
 var app = http.createServer(function(request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query; // url의 query string을 파싱
-  var title = queryData.id;
-  if (_url == "/") {
-    title = "Welcome"; // root 경로로 접속했을 때, title을 Welcome으로 할 거임
-  }
-  if (_url == "/favicon.ico") {
+  var pathname = url.parse(_url, true).pathname;
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      // root 경로 이고, query string이 없으면 ('http://localhost:3000/')
+      fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p> 
+        </body>
+        </html>
+        `; // data 폴더에 있는 파일 내용을 본문에 삽입
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      // root 경로이고, query string이 있을 때 ('http://localhost:3000/?id=HTML')
+      fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
+        var title = queryData.id;
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p> 
+        </body>
+        </html>
+        `; // data 폴더에 있는 파일 내용을 본문에 삽입
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+  } else {
     response.writeHead(404);
-    response.end();
-    return;
+    response.end("Not found");
   }
-  response.writeHead(200);
-  fs.readFile(`data/${title}`, "utf8", function(err, description) {
-    var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p> 
-      </body>
-      </html>
-      `; // data 폴더에 있는 파일 내용을 본문에 삽입
-    response.end(template);
-  });
 });
 app.listen(3000);
