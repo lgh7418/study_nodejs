62fe6913bd3a2062c5abec2afea5cfac164a44d3
lgh7418 <lgh7418@gmial.com>
2019-04-15 22:33:12 +0900

    24. 글 목록 출력하기

--- main.js
+++ main.js
@@ -7,14 +7,19 @@ var app = http.createServer(function(request, response) {
   var queryData = url.parse(_url, true).query; // url의 query string을 파싱
   var pathname = url.parse(_url, true).pathname;
   if (pathname === "/") {
-    fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
-      if (queryData.id === undefined) {
+    if (queryData.id === undefined) {
+      fs.readdir("./data", function(err, filelist) {
         var title = "Welcome";
         var description = "Hello, Node.js";
-      } else {
-        var title = queryData.id;
-      }
-      var template = `
+        var list = "<ul>";
+        var i = 0;
+        while (i < filelist.length) {
+          list =
+            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + "</ul>";
+        var template = `
         <!doctype html>
         <html>
         <head>
@@ -23,19 +28,47 @@ var app = http.createServer(function(request, response) {
         </head>
         <body>
           <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
+          ${list}
           <h2>${title}</h2>
           <p>${description}</p> 
         </body>
         </html>
         `;
-      response.writeHead(200);
-      response.end(template);
-    });
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readdir("./data", function(err, filelist) {
+        var list = "<ul>";
+        var i = 0;
+        while (i < filelist.length) {
+          list =
+            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + "</ul>";
+        fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
+          var title = queryData.id;
+          var template = `
+         <!doctype html>
+         <html>
+         <head>
+           <title>WEB1 - ${title}</title>
+           <meta charset="utf-8">
+         </head>
+         <body>
+           <h1><a href="/">WEB</a></h1>
+           ${list}
+           <h2>${title}</h2>
+           <p>${description}</p> 
+         </body>
+         </html>
+         `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    }
   } else {
     response.writeHead(404);
     response.end("Not found");
