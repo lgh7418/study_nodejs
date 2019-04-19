736da52085df9f00595d27916b8b42d58687848f
lgh7418 <lgh7418@gmial.com>
2019-04-19 10:35:30 +0900

    6. MySQL로 상세보기, 글생성 기능 구현

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ var qs = require("querystring");
 var template = require("./lib/template.js");
 var path = require("path");
 var sanitizeHTML = require("sanitize-html"); // html 태그를 sanitize해줌
+var mysql = require("mysql");
+var db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "opentutorials"
+});
+db.connect();
 
 var app = http.createServer(function(request, response) {
   var _url = request.url;
@@ -12,10 +20,10 @@ var app = http.createServer(function(request, response) {
   var pathname = url.parse(_url, true).pathname;
   if (pathname === "/") {
     if (queryData.id === undefined) {
-      fs.readdir("./data", function(err, filelist) {
+      db.query("SELECT * FROM topic", function(error, topics) {
         var title = "Welcome";
         var description = "Hello, Node.js";
-        var list = template.list(filelist);
+        var list = template.list(topics);
         var html = template.HTML(
           title,
           list,
@@ -26,7 +34,7 @@ var app = http.createServer(function(request, response) {
         response.end(html);
       });
     } else {
-      fs.readdir("./data", function(err, filelist) {
+      /* fs.readdir("./data", function(err, filelist) {
         var filteredId = path.parse(queryData.id).base;
         fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
           var title = queryData.id;
@@ -49,12 +57,42 @@ var app = http.createServer(function(request, response) {
           response.writeHead(200);
           response.end(html);
         });
+      }); */
+      db.query("SELECT * FROM topic", function(error, topics) {
+        if (error) {
+          throw error;
+        }
+        // `SELECT * FROM topic WHERE id=${queryData.id}` 이렇게 쓰면 사용자 입력값에 공격 당할 위험이 있음
+        // ? 이용하면 queryData.id 값이 ?에 치환될 때 자동으로 세탁해줌
+        db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic) {
+          if (error2) {
+            throw error2;
+          }
+          var title = topic[0].title;
+          var description = topic[0].description;
+          var list = template.list(topics);
+          var html = template.HTML(
+            title,
+            list,
+            `<h2>${title}</h2><p>${description}</p> `,
+            `
+            <a href="/create">create</a>
+            <a href="/update?id=${queryData.id}">update</a>
+            <form action="delete_process" method="post">
+              <input type="hidden" name="id" value="${queryData.id}">
+              <input type="submit" value="delete">
+            </form>
+            `
+          );
+          response.writeHead(200);
+          response.end(html);
+        });
       });
     }
   } else if (pathname === "/create") {
-    fs.readdir("./data", function(err, filelist) {
+    db.query("SELECT * FROM topic", function(error, topics) {
       var title = "WEB - create";
-      var list = template.list(filelist);
+      var list = template.list(topics);
       var html = template.HTML(
         title,
         list,
@@ -83,15 +121,19 @@ var app = http.createServer(function(request, response) {
     });
     // 더이상 전송할 데이터가 없으면 다음 함수를 실행
     request.on("end", function() {
-      var post = qs.parse(body); // body를 파싱해서 객체화할 수 있음
-      console.log(post);
-      var title = post.title;
-      var description = post.description;
-      // data 디렉토리에 파일 생성
-      fs.writeFile(`data/${title}`, description, "utf8", function(err) {
-        response.writeHead(302, { Location: `/?id=${title}` }); // 파일 생성이 끝나면 리다이렉트
-        response.end();
-      });
+      var post = qs.parse(body);
+      db.query(`INSERT INTO topic (title, description, created, author_id) 
+        VALUES(?, ?, NOW(), ?)`,
+        [post.title, post.description, 1],
+        function(error, result){
+          if(error){
+            throw error;
+          }
+          // id가 auto increment primary key인 경우, insert한 row의 id를 받아옴 => result.insertId
+          response.writeHead(302, { Location: `/?id=${result.insertId}` });
+          response.end();
+        }
+      )
     });
   } else if (pathname === "/update") {
     fs.readdir("./data", function(err, filelist) {
